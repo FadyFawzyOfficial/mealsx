4b706dcc1bf4c8a89a3b8aa038e798c768f98e70
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2024-08-13 07:25:44 +0300

    30. Add more (remaining) filter options to filter meals

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -14,6 +14,9 @@ class FiltersScreen extends StatefulWidget {
 
 class _FiltersScreenState extends State<FiltersScreen> {
   var isGlutenFree = false;
+  var isLactoseFree = false;
+  var isVegetarian = false;
+  var isVegan = false;
 
   @override
   Widget build(context) {
@@ -34,7 +37,25 @@ class _FiltersScreenState extends State<FiltersScreen> {
             onChanged: (isChecked) => setState(() => isGlutenFree = isChecked),
             title: 'Gluten-free',
             subTitle: 'Only include gluten-free meals.',
-          )
+          ),
+          SwitchFilter(
+            value: isLactoseFree,
+            onChanged: (isChecked) => setState(() => isLactoseFree = isChecked),
+            title: 'Lactose-free',
+            subTitle: 'Only include lactose-free meals.',
+          ),
+          SwitchFilter(
+            value: isVegetarian,
+            onChanged: (isChecked) => setState(() => isVegetarian = isChecked),
+            title: 'Vegetarian',
+            subTitle: 'Only include vegetarian meals.',
+          ),
+          SwitchFilter(
+            value: isVegan,
+            onChanged: (isChecked) => setState(() => isVegan = isChecked),
+            title: 'Vegan',
+            subTitle: 'Only include vegan meals.',
+          ),
         ],
       ),
     );
