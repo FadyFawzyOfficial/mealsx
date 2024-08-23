5750bc37cc846f94845d8abc61f660b450bfe64d
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2024-08-23 11:13:23 +0300

    2. Convert FilterScreen to extends from ConsumerStatefulWidget instead of StatefulWidget

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../constants/strings.dart';
 import '../providers/filter_provider.dart';
@@ -6,16 +7,16 @@ import '../widgets/switch_filter.dart';
 // import '../widgets/app_drawer.dart';
 // import 'tabs_screen.dart';
 
-class FiltersScreen extends StatefulWidget {
+class FiltersScreen extends ConsumerStatefulWidget {
   final Map<Filter, bool> mealsFilter;
 
   const FiltersScreen({super.key, required this.mealsFilter});
 
   @override
-  State<FiltersScreen> createState() => _FiltersScreenState();
+  ConsumerState<FiltersScreen> createState() => _FiltersScreenState();
 }
 
-class _FiltersScreenState extends State<FiltersScreen> {
+class _FiltersScreenState extends ConsumerState<FiltersScreen> {
   late Map<Filter, bool> mealsFilter;
 
   @override
