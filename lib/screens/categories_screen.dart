dc72f831726cc5f4af5fbe8201fffa592c114fc0
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2024-08-23 15:03:36 +0300

    1. Convert CategoriesScreen to StatefulWidget instead fo StatelessWidget

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -3,12 +3,17 @@ import 'package:flutter/material.dart';
 import '../models/meal.dart';
 import '../widgets/categories_grid_view.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   final List<Meal> filteredMeals;
 
   const CategoriesScreen({super.key, required this.filteredMeals});
 
   @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen> {
+  @override
   Widget build(context) =>
-      Scaffold(body: CategoriesGridView(filteredMeals: filteredMeals));
+      Scaffold(body: CategoriesGridView(filteredMeals: widget.filteredMeals));
 }
