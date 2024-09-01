d81ee2eaead64b4d2968d93420b3f6693e2a66d9
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2024-09-01 21:43:25 +0300

    7. Add multi-screen transition animation (Implement Hero animation for meals images)

--- lib/screens/meal_screen.dart
+++ lib/screens/meal_screen.dart
@@ -35,11 +35,14 @@ class MealScreen extends ConsumerWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Image.network(
-              meal.imageUrl,
-              height: 250,
-              width: double.infinity,
-              fit: BoxFit.cover,
+            Hero(
+              tag: meal.id,
+              child: Image.network(
+                meal.imageUrl,
+                height: 250,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
             ),
             const SizedBox(height: 16),
             Text(
